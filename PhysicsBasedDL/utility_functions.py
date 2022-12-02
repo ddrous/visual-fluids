0adfab96aeb3ecc903eca3c2d1f2ab1ebc0ce8de
RDN (Mac) <desmond.ngueguin@gmail.com>
2022-12-02 05:54:32 +0000

    Made custom collate function for Dataloaders

--- PhysicsBasedDL/utility_functions.py
+++ PhysicsBasedDL/utility_functions.py
@@ -1,21 +1,26 @@
 import numpy as np
-import matplotlib
-matplotlib.style.use('seaborn')
+
+# import seaborn as sns
+# sns.set(context='poster', style='white',
+#         font='sans-serif', font_scale=1, color_codes=True, None)
+# sns.despine()
+
 import matplotlib.pyplot as plt
+plt.style.use('bmh')
 
 ## Wrapper function for matplotlib
-def plot(*args, axis=None, x_label=None, y_label=None, title=None, **kwargs):
-    if axis==None: 
-        _, axis = plt.subplots(1, 1, figsize=(6,3))
+def plot(*args, ax=None, x_label=None, y_label=None, title=None, **kwargs):
+    if ax==None: 
+        _, ax = plt.subplots(1, 1, figsize=(8,5))
     if x_label:
-        axis.set_xlabel(x_label)
+        ax.set_xlabel(x_label)
     if y_label:
-        axis.set_ylabel(y_label)
+        ax.set_ylabel(y_label)
     if title:
-        axis.set_title(title)
-    axis.plot(*args, **kwargs)
-    axis.legend()
-    return axis
+        ax.set_title(title)
+    ax.plot(*args, **kwargs)
+    ax.legend(fontsize=12)
+    return ax
 
 ## Data generator
 def get_batch_data(X, Y, batch_size):
@@ -26,3 +31,9 @@ def get_batch_data(X, Y, batch_size):
         if inext > size: inext = size
         yield X[i:inext], Y[i:inext]
         i = inext
+
+## A function for the Pytorch dataloader that will return data as NumPy arrays
+def numpy_collate_fn(batch):
+    inputs, outputs = zip(*batch)
+    ## NB. Make sure each element if numpy tensor first
+    return np.stack(inputs), np.stack(outputs)
